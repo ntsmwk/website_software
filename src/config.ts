478d44ff5ed74f48439b2d6b2e7829c4abed323f
Markus Weißenbek <markus.weissenbek@gmx.at>
2025-02-04 14:05:16 +0100

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Weißenbek Software';
+export const SITE_DESCRIPTION = 'Website of Weißenbek Software e.U.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
